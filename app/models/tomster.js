7a857eee0c43586cab172850ffc55f9c5f4ae5a5
Chris McCuller <chris.mcculler@gmail.com>
2018-09-13 22:23:33 -0400

    finally get things working

--- app/models/tomster.js
+++ app/models/tomster.js
@@ -1,5 +1,5 @@
 import Model from 'ember-data/model';
-import attr from '@ember-decorators/attr';
+import { attr } from '@ember-decorators/data';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default class Tomster extends Model {
